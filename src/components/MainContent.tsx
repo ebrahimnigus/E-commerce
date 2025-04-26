3ac198a29be86ee9e7597adf481ba0bd9c80549f
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-26 10:22:16 +0300

    make sidebar functional

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -8,6 +8,7 @@ const MainContent = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [dropdownOpen, setDropdownOpen]= useState(false);
     const itemsPerPage = 12;
+    
   return (
     <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
         <div className="mb-5">
