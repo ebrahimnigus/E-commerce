2c7403c6beae5e1fe3bcc17f02685dc2f7b4b109
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-28 23:21:00 +0300

    updated products

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-interface BookCardPage {
+interface BookCardProps {
     id: string;
     title: string;
     image: string;
@@ -10,9 +9,8 @@ interface BookCardPage {
 
 const BookCard: React.FC<BookCardProps> = ({ id, title, image, price }) => {
     return <div className="border p-4 rounded">
-        <Link to={'/product/${id}'}>
+        <Link to={`/product/${id}`}>
             <img src={image} alt={title} className="w-full h-32 object-cover mb-2" />
-
             <h2 className="font-bold">
                 {title}
             </h2>
