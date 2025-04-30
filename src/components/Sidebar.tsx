dbc632dafef40f1205ba68a938d634a127f8815b
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-30 12:22:37 +0300

    update sidebar styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -81,14 +81,14 @@ const Sidebar = () => {
   }
 
 
-  return <div className="w-64 p-5 h-screen">
+  return <div className="w-72 p-5 h-screen">
     <h1 className="text-2xl font-bold mb-10 mt-4">
       React Store
     </h1>
     <section>
-      <input type="text" className="border-2 rounded px-2 sm:mb-0" placeholder="Search Product" value={searchQuery} onChange={e=>setSearchQuery(e.target.value)}/>
+      <input type="text" className="border-2 rounded px-2 py-3 w-full sm:mb-0" placeholder="Search Product" value={searchQuery} onChange={e=>setSearchQuery(e.target.value)}/>
 
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center mt-3 items-center">
         <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder="Min" value={minPrice ?? ''} onChange={handleMinPriceChange}/>
         <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder="Max" value={maxPrice ?? ''} onChange={handleMaxPriceChange} />
       </div>
